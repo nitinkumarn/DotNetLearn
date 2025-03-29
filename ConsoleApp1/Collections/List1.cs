735cc04df61598f64547b8d19192be2fa6a6f1a2
Nitinkumar N <nitinkumarsuccess@gmail.com>
2025-03-29 13:35:12 +0530

    Added One list program

---
+++ ConsoleApp1/Collections/List1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Collections
+{
+    class List1
+    {
+        static void Main(string[] args)
+        {
+            List<String> li = new List<string> {"Bangalore","Chennai","Mumbai","Delhi"};
+
+            int index = li.IndexOf("Chennai");
+
+            if (index != -1)
+            {
+                li[index] = "Pune";
+            }
+            else
+            {
+                throw new Exception("Element not present ");
+            }
+            Console.WriteLine("The replaced city list are: ");
+            foreach (String s in li)
+            {
+                
+                Console.WriteLine(s);
+            }
+
+            
+            
+            
+        }
+    }
+}
